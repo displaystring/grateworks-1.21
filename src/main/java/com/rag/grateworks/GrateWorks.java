79bbf719d15cf5843718fe16f3704153d7d2d050
displaystring <rafael.danvers@gmail.com>
2025-11-23 14:07:28 +1100

    this is annoying to handle >:(

--- src/main/java/com/rag/grateworks/GrateWorks.java
+++ src/main/java/com/rag/grateworks/GrateWorks.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// yeah
 public class GrateWorks implements ModInitializer {
 	public static final String MOD_ID = "grateworks";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
