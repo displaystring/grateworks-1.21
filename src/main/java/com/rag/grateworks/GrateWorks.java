cd0bbf7f83e6c95687e329a1aa6bddaf2667dea0
displaystring <rafael.danvers@gmail.com>
2025-11-23 14:04:50 +1100

    adding stuff

---
+++ src/main/java/com/rag/grateworks/GrateWorks.java
@@ -0,0 +1,16 @@
+package com.rag.grateworks;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GrateWorks implements ModInitializer {
+	public static final String MOD_ID = "grateworks";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/rag/grateworks/GrateWorksClient.java
@@ -0,0 +1,10 @@
+package com.rag.grateworks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GrateWorksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
